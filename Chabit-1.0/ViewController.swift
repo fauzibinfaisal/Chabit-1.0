243b85bd62a26691b698e647bd208d34f7f1601d
Fauzi <fauzibinfaisal@gmail.com>
2019-07-18 16:53:08 +0700

    Initial Commit

---
+++ Chabit-1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chabit-1.0
+//
+//  Created by Fauzi Fauzi on 18/07/19.
+//  Copyright © 2019 C2G10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
