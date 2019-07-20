b46e94d974342cb154b1f251b46fff23d6965242
Fauzi <fauzibinfaisal@gmail.com>
2019-07-20 15:11:57 +0700

    add play sound

--- Chabit-1.0/Controller/SoundOfNatureViewController.swift
+++ Chabit-1.0/Controller/SoundOfNatureViewController.swift
@@ -11,28 +11,99 @@ import AVFoundation
 
 class SoundOfNatureViewController: UIViewController {
     
-    let nameSounds = ["Cast of Pods","","Anthem of Nature","Waterfall Rhapsody"]
-    let soundDurations = ["4 min","3 min", "6 min"]
-    let soundNames = ["cast_of_pods","cast_of_pods", "cast_of_pods"]
+    var player : AVAudioPlayer?
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var soundsCollectionView: UICollectionView!
+    
+    var nameSounds = ["Cast of Pods","Anthem of Nature","Waterfall Rhapsody"]
+    var soundDurations = ["4 min","3 min", "6 min"]
+    var soundNames = ["pods","rain", "pods"]
+    var isPlayed: Bool = false
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let player : AVAudioPlayer?
+        stopButton.alpha = 0
         
-        let url = Bundle.main.url(forResource: "name_file", withExtension: "mp3")!
+        soundsCollectionView.delegate = self
+        soundsCollectionView.dataSource = self
         
-        do{
-            player = try AVAudioPlayer(contentsOf: url)
-            player?.prepareToPlay()
-            player?.play()
-        }
-        catch{
-            print(error)
-            
-        }
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func stopButtonAction(_ sender: UIButton) {
+        player?.stop()
+        stopButton.alpha = 0
+        isPlayed = false
+    }
+    
+//    func playMusic(nameMusic: String) {
+//    func playMusic() {
+//        let player : AVAudioPlayer?
+//
+//        let url = Bundle.main.url(forResource: "Cast_of_Pods", withExtension: ".mp3")!
+//
+//        do{
+//            player = try AVAudioPlayer(contentsOf: url)
+//            player?.prepareToPlay()
+//            player?.play()
+//        }
+//        catch{
+//            print(error)
+//
+//        }
+//    }
+    
+    func playSound(fileName: String) {
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension SoundOfNatureViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return nameSounds.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "soundCell", for: indexPath) as? SoundCell
+        cell?.soundNameLabel.text = nameSounds[indexPath.row]
+        cell?.soundTimeLabel.text = soundDurations[indexPath.row]
+        
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(soundNames[indexPath.row])
+        if (!isPlayed){
+            isPlayed = true
+            playSound(fileName: soundNames[indexPath.row])
+            stopButton.alpha = 1
+        } else {
+            stopButton.alpha = 0
+        }
+
+//        playMusic(nameMusic: soundNames[indexPath.row])
+
+    }
     
 }
---
+++ Chabit-1.0/Model/SoundCell.swift
@@ -0,0 +1,18 @@
+//
+//  DataCollectionViewCell.swift
+//  Collection
+//
+//  Created by Fauzi Fauzi on 28/04/19.
+//  Copyright © 2019 Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class SoundCell: UICollectionViewCell {
+
+    @IBOutlet weak var soundNameLabel: UILabel!
+    
+    @IBOutlet weak var soundTimeLabel: UILabel!
+    
+    
+}
