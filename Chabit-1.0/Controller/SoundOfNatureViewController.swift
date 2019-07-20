8c06143ad3171e1ce32fcfbfbf8f85b6bbbc2c2a
Fauzi <fauzibinfaisal@gmail.com>
2019-07-20 12:59:45 +0700

    add sound

--- Chabit-1.0/Controller/SoundOfNatureViewController.swift
+++ Chabit-1.0/Controller/SoundOfNatureViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundOfNatureViewController: UIViewController {
     
     let nameSounds = ["Cast of Pods","","Anthem of Nature","Waterfall Rhapsody"]
     let soundDurations = ["4 min","3 min", "6 min"]
     let soundNames = ["cast_of_pods","cast_of_pods", "cast_of_pods"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let player : AVAudioPlayer?
+        
+        let url = Bundle.main.url(forResource: "name_file", withExtension: "mp3")!
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+            player?.play()
+        }
+        catch{
+            print(error)
+            
+        }
         // Do any additional setup after loading the view.
     }
-    
     
     
 }
