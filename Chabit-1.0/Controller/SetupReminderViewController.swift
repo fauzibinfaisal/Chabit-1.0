7b953cbeda155a0cbdd565f7e4299c8d9cd441d9
Fauzi <fauzibinfaisal@gmail.com>
2019-07-20 15:43:24 +0700

    user local notification edit on boarding

--- Chabit-1.0/Controller/SetupReminderViewController.swift
+++ Chabit-1.0/Controller/SetupReminderViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import UserNotifications
 
 class SetupReminderViewController: UIViewController, UITextFieldDelegate {
-
+    
+    var hour:Int = 0
+    var minute:Int = 0
+    
     @IBOutlet weak var reminderTF: UITextField!{
         didSet{
             reminderTF.borderStyle = .roundedRect
@@ -20,6 +24,11 @@ class SetupReminderViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //         Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+        }
         reminderTF.delegate = self
         
         let toolBar = UIToolbar().ToolbarPiker(mySelect: #selector(SetupReminderViewController.dismissPicker))
@@ -38,13 +47,17 @@ class SetupReminderViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    @IBAction func setTimePicker(_ sender: UIButton) {
+        setReminderCustom(hour: hour, minute: minute)
+    }
     
     @IBAction func reminderTFEdit(_ sender: UITextField) {
+        
         let datePickerView:UIDatePicker = UIDatePicker()
         
-        datePickerView.datePickerMode = UIDatePicker.Mode.time
-        
+        datePickerView.datePickerMode = .time
         sender.inputView = datePickerView
+        
         
         datePickerView.addTarget(self, action: #selector(SetupReminderViewController.datePickerValueChanged), for: UIControl.Event.valueChanged)
     }
@@ -52,24 +65,116 @@ class SetupReminderViewController: UIViewController, UITextFieldDelegate {
     
     @objc func datePickerValueChanged(sender:UIDatePicker) {
         
+        let datePicker = sender.date
+        let date = Calendar.current.dateComponents([.hour, .minute], from: datePicker)
+        
+        hour = date.hour!
+        minute = date.minute!
+        
+        print("\(hour) : \(minute)")
+        
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        dateFormatter.timeStyle = DateFormatter.Style.medium
+        dateFormatter.dateFormat = "hmm"
         reminderTF.text = dateFormatter.string(from: sender.date)
         
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    func setReminderCustom(hour:Int,minute:Int){
+        
+        
+        var contentMorning = UNMutableNotificationContent()
+        var contentAfternoon = UNMutableNotificationContent()
+        var contentNight = UNMutableNotificationContent()
+        
+        
+        //Content Pagi
+        contentMorning.title = "Don't Forget Hadi!"
+        contentMorning.body = "6 Activity Left, Check it out !"
+        contentMorning.subtitle = "Morning Routine Activity"
+        contentMorning.sound = .default
+        
+        //Content Siang
+        contentAfternoon.title = "Don't Forget Hadi!"
+        contentAfternoon.body = "6 Activity Left, Check it out !"
+        contentAfternoon.subtitle = "Afternoon Routine Activity"
+        contentAfternoon.sound = .default
+        
+        //Content Malem
+        contentNight.title = "Don't Forget Hadi!"
+        contentNight.body = "6 Activity Left, Check it out !"
+        contentNight.subtitle = "Night Routine Activity"
+        contentNight.sound = .default
+        
+        let gregorian = Calendar(identifier: .gregorian)
+        let now = Date()
+        
+        var componentMorning = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        // Change the time to 7:00:00 in your locale
+        componentMorning.hour = hour
+        componentMorning.minute = minute
+        componentMorning.second = 10
+        
+        var componentAfternoon = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        // Change the time to 7:00:00 in your locale
+        componentAfternoon.hour = 17
+        componentAfternoon.minute = 52
+        componentAfternoon.second = 30
+        
+        var componentNight = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        // Change the time to 7:00:00 in your locale
+        componentNight.hour = 17
+        componentNight.minute = 52
+        componentNight.second = 40
+        
+        
+        let dateMorning = gregorian.date(from: componentMorning)!
+        let dateAfternoon = gregorian.date(from: componentAfternoon)!
+        let dateNight = gregorian.date(from: componentNight)!
+        
+        let triggerDailyMorning = Calendar.current.dateComponents([.hour,.minute,.second,], from: dateMorning)
+        let triggerDailyAfternoon = Calendar.current.dateComponents([.hour,.minute,.second,], from: dateAfternoon)
+        let triggerDailyNight = Calendar.current.dateComponents([.hour,.minute,.second,], from: dateNight)
+        
+        let triggerMorning = UNCalendarNotificationTrigger(dateMatching: triggerDailyMorning, repeats: true)
+        let triggerAfternoon = UNCalendarNotificationTrigger(dateMatching: triggerDailyAfternoon, repeats: true)
+        let triggerNight = UNCalendarNotificationTrigger(dateMatching: triggerDailyNight, repeats: true)
+        
+        let uuidString1 = UUID().uuidString
+        let uuidString2 = UUID().uuidString
+        let uuidString3 = UUID().uuidString
+        
+        let requestMorning = UNNotificationRequest(identifier: uuidString1, content: contentMorning, trigger: triggerMorning)
+        let requestAfternoon = UNNotificationRequest(identifier: uuidString2, content: contentAfternoon, trigger: triggerAfternoon)
+        let requestNight = UNNotificationRequest(identifier: uuidString3, content: contentNight, trigger: triggerNight)
+        
+        let center = UNUserNotificationCenter.current()
+        
+        print("INSIDE NOTIFICATION")
+        
+        center.add(requestMorning) { (error) in
+            //             Check the error parameter and handle any errors
+        }
+        center.add(requestAfternoon) { (error) in
+            //             Check the error parameter and handle any errors
+        }
+        center.add(requestNight) { (error) in
+            //             Check the error parameter and handle any errors
+        }
+        
     }
-    */
-
+    
 }
 
 extension UIToolbar {
